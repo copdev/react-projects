a83bd30a55281c6bcedebf310207b15a9449be6c
copdev <glovermartial@gmail.com>
2021-01-05 11:24:09 +0100

    Tabs

--- 1-birthday-reminder/setup/src/App.js
+++ 1-birthday-reminder/setup/src/App.js
@@ -1,8 +1,22 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
-function App() {
-  return <h2>reminder project setup</h2>;
+function App () {
+  const [ people, setPeople ] = useState( data );
+
+  const handleRemove = (id) => {
+    const newPeople = people.filter((person) => { person.id !== id;
+      setPeople(newPeople);
+    });
+  };
+  
+  return <main>
+    <section className="container">
+      <h3>{people.length} birthdays today</h3>
+      <List people={people} handleRemove />
+      <button onClick={() => setPeople([])}>Clear all</button>
+    </section>
+  </main>;
 }
 
 export default App;
--- 4-accordion/setup/src/App.js
+++ 4-accordion/setup/src/App.js
@@ -1,8 +1,23 @@
 import React, { useState } from 'react';
 import data from './data';
-import SingleQuestion from './Question';
-function App() {
-  return <h2>accordion project setup</h2>;
+import Question from './Question';
+function App () {
+  const [ questions, setQuestions ] = useState( data );
+  console.log( questions )
+  
+  return <main>
+    <div className="container">
+      <h3>questions and answers about login</h3>
+      <section className="info">
+        {
+          questions.map( ( question ) => {
+            return <Question key={question.id} {...question} />;
+          }) 
+        }
+      </section>
+    </div>
+    
+  </main>;
 }
 
 export default App;
