7e1d03b827f7bb48a60f7a0d78eb8234a94e1443
copdev <glovermartial@gmail.com>
2021-02-18 09:20:18 +0100

    grocery bud

--- 10-grocery-bud/setup/src/Alert.js
+++ 10-grocery-bud/setup/src/Alert.js
@@ -4,4 +4,4 @@ const Alert = () => {
   return <h2>alert component</h2>
 }
 
-export default Alert
+export default Alert;
--- 10-grocery-bud/setup/src/List.js
+++ 10-grocery-bud/setup/src/List.js
@@ -4,4 +4,4 @@ const List = () => {
   return <h2>list component</h2>
 }
 
-export default List
+export default List;
